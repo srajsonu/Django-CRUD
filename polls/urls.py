4be022300fdf07aef5a414abe08363034bb5c478
SONU RAJ <srajsonu02@gmail.com>
2020-09-25 00:20:32 +0530

    week 4 completed

--- polls/urls.py
+++ polls/urls.py
@@ -5,12 +5,21 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('owner/', views.owner, name='owner'),
+    # path('', views.index, name='index'),
+    # path('owner/', views.owner, name='owner'),
+    #
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # path('specifics/<int:question_id>/', views.detail, name='detail'),
+    #
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
 
-    #path('<int:question_id>/', views.detail, name='detail'),
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
 
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.results, name='vote'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
