58bfa1df607d88aa9a0a0b339bed1d13a25f32fa
SONU RAJ <srajsonu02@gmail.com>
2020-09-25 00:25:41 +0530

    minor change

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,7 @@ app_name = 'polls'
 
 urlpatterns = [
     # path('', views.index, name='index'),
-    # path('owner/', views.owner, name='owner'),
+    path('owner/', views.owner, name='owner'),
     #
     # path('<int:question_id>/', views.detail, name='detail'),
     # path('specifics/<int:question_id>/', views.detail, name='detail'),
