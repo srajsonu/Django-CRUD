ba09b75ff9d43e3e52e1c9354d2bf551c8d86a46
SONU RAJ <srajsonu02@gmail.com>
2020-09-24 07:27:22 +0000

    vews and models added

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('owner/', views.owner, name='owner'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.results, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. cc5f52c9 is the polls index.")
+
+def owner(request):
+       return HttpResponse("Hello, world. 198dd5fb is the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
