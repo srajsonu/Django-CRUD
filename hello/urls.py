a1df82a655cd5c2c771671884cae21b45a8c3a87
SONU RAJ <srajsonu02@gmail.com>
2020-09-25 23:55:00 +0530

    minor change

--- hello/urls.py
+++ hello/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='hello/main.html')),
     path('hello', views.cookie),
     path('polls', views.session),
+    path('polls/owner', views.owner),
 ]
