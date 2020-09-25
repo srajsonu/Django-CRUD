a9e2d92672bd41e2e2f6bd70142eae8b82c988a3
SONU RAJ <srajsonu02@gmail.com>
2020-09-25 21:44:55 +0530

    session added

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def cookie(request):
+    print(request.COOKIES)
+
+
+def session(request):
+    num_of_vis = request.session.get('num_of_vis', 0) + 1
+    request.session['num_of_vis'] = num_of_vis
+    if num_of_vis > 4:
+        del request.session['num_of_vis']
+
+    return HttpResponse('view count='+str(num_of_vis))
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'hello.apps.HelloConfig',
     'home.apps.HomeConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
