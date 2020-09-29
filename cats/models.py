932269c60deda24317b318cb12dce8c8cbc6d16a
SONU RAJ <srajsonu02@gmail.com>
2020-09-29 16:42:38 +0000

    new app added

---
+++ cats/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Breed(models.Model):
+    pass
+
+class Cat(models.Model):
+    pass
--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'cats.apps.CatsConfig',
     'autos.apps.AutosConfig',
     'hello.apps.HelloConfig',
     'home.apps.HomeConfig',
