022d3090f1a7e62aff525b3f44291a39aaa99914
SONU RAJ <srajsonu02@gmail.com>
2020-10-19 22:39:06 +0530

    new branch created and Ads app modified

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,8 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+
+import ads
+from ads.forms import CreateForm
 from ads.models import Ad
 from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
@@ -13,13 +18,50 @@ class AdDetailView(OwnerDetailView):
 
 
 class AdCreateView(OwnerCreateView):
-    model = Ad
-    fields = ['title', 'text', 'price']
+    template_name = 'ads/ad_detail.html'
+    success_url = reverse_lazy('ads:all')
+
+    def get(self, request, pk=None):
+        form = CreateForm()
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        form = CreateForm(request.POST, request.FILES or None)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        pic = form.save(commit=False)
+        pic.owner = self.request.user
+        pic.save()
+        return redirect(self.success_url)
+
 
 
 class AdUpdateView(OwnerUpdateView):
-    model = Ad
-    fields = ['title', 'text']
+    template_name = 'ads/ad_form.html'
+    success_url = reverse_lazy('pics:all')
+
+    def get(self, request, pk):
+        pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(instance=pic)
+        ctx = {'form': form}
+        return render(request, self.template_name, ctx)
+
+    def post(self, request, pk=None):
+        pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=pic)
+
+        if not form.is_valid():
+            ctx = {'form': form}
+            return render(request, self.template_name, ctx)
+
+        pic = form.save(commit=False)
+        pic.save()
+
+        return redirect(self.success_url)
 
 
 class AdDeleteView(OwnerDeleteView):
