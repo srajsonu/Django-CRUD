96bcf5e1c614e7713e32f45b854853964fafd78f
srajsonu <srajsonu02@gmail.com>
2020-11-12 16:57:17 +0000

    minor changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 
     # Extensions - installed with pip3 / requirements.txt
     'django_extensions',
-    #'crispy_forms',
+    'crispy_forms',
     'rest_framework',
     'social_django',
     'home.apps.HomeConfig',
