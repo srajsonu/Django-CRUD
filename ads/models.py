d92dc99b451d9bbdc6ed89bda7e741478cfc45fa
SONU RAJ <srajsonu02@gmail.com>
2020-11-14 13:35:56 +0530

    ads app updated

--- ads/models.py
+++ ads/models.py
@@ -25,32 +25,32 @@ class Ad(models.Model) :
     def __str__(self):
         return self.title
 
-class Forum(models.Model) :
-    title = models.CharField(
-            max_length=200,
-            validators=[MinLengthValidator(5, "Title must be greater than 5 characters")]
-    )
-    text = models.TextField()
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE, related_name='forums_owned')
-
-    #user = models.ForeignKey('User', on_delete=models.CASCADE)
-    comments = models.ManyToManyField(settings.AUTH_USER_MODEL,
-        through='Comment', related_name='forum_comments')
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    # Shows up in the admin list
-    def __str__(self):
-        return self.title
+# class Forum(models.Model) :
+#     title = models.CharField(
+#             max_length=200,
+#             validators=[MinLengthValidator(5, "Title must be greater than 5 characters")]
+#     )
+#     text = models.TextField()
+#     owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE, related_name='forums_owned')
+#
+#     #user = models.ForeignKey('User', on_delete=models.CASCADE)
+#     comments = models.ManyToManyField(settings.AUTH_USER_MODEL,
+#         through='Comment', related_name='forum_comments')
+#
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#
+#     # Shows up in the admin list
+#     def __str__(self):
+#         return self.title
 
 class Comment(models.Model) :
     text = models.TextField(
         validators=[MinLengthValidator(3, "Comment must be greater than 3 characters")]
     )
 
-    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    forum = models.ForeignKey(Ad, on_delete=models.CASCADE)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
